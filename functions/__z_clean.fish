0deefdb4f00b7237f56fb899a639d145dd43238d
Daniel Bretoi <daniel@otherware.org>
2026-09-21 13:25:21 +0700

    Preserve encoded paths during cleanup

--- functions/__z_clean.fish
+++ functions/__z_clean.fish
@@ -8,8 +8,12 @@ function __z_clean -d "Clean up z data to remove paths no longer valid"
         end
 
         set -l path (__z_decode_path "$fields[1]")
+        set -l stored_path "$fields[1]"
+        if not string match -q 'v1:*' -- "$stored_path"
+            set stored_path (__z_encode_path "$path")
+        end
         if test -d "$path"
-            printf "%s|%s|%s\n" (__z_encode_path "$path") "$fields[2]" "$fields[3]"
+            printf "%s|%s|%s\n" "$stored_path" "$fields[2]" "$fields[3]"
         end
     end < "$Z_DATA" > "$tmpfile"; or return 1
 
